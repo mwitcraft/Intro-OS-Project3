b2041a1f9d6cfba119b4b82b817e774c60048720
Mason <mwitcraft11@gmail.com>
2018-11-01 16:43:46 -0500

    In zformat, initialized all bytes in master to 0

---
+++ zformat.c
@@ -0,0 +1,35 @@
+// Write 0s to all the bytes in the virtual disk
+// Initialize master block
+//    -Mark master block, all inode blocks, and first data block (root directory) as allocated
+//    -Mark the first inode as allocated (root)
+// Initialize the first inode (root)
+// Initialize the first data block as an empty directory, but with '.' and '..' referring both to inode 0
+
+#include <stdio.h>
+#include <string.h>
+
+#include "Given/oufs_lib.h"
+#include "Given/vdisk.h"
+
+int main(int argc, char** argv){
+    // Creates a virtual disk with name 'vdisk1'
+    if(vdisk_disk_open("vdisk1") != 0)
+      return -1;
+
+    //Step through all blocks in virtual disk
+    for(int i = 0; i < 128; ++i){
+      BLOCK block;
+      // Master block
+      if(i == 0){
+        // Steps through each byte in the block and sets it to 0
+        for(int j = 0; j < 8; ++j){
+          block.master.inode_allocated_flag[j] = 0;
+          block.master.block_allocated_flag[j] = 0;
+        }
+        if(vdisk_write_block(0, &block) != 0){
+          // TODO: add error check
+        }
+      }
+      
+    }
+}
